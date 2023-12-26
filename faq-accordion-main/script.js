23e1b473111da7febac2bd41fb17b482cbdc83bc
vcgmuse <vcgmuse@gmail.com>
2023-12-26 17:36:54 -0600

    Created and uploaded first challenge and main folder.

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,4 +1,44 @@
 const allImageBtn = document.querySelectorAll('.card');
+const allRows = document.querySelectorAll('.row');
+let rowIndex = 0;
+const keys = document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  allRows.forEach((row) => {
+    row.classList.remove('selected');
+  });
+  console.log(e.key);
+  switch (e.key) {
+    case 'ArrowUp':
+      rowIndex = rowIndex > 1 ? rowIndex - 1 : 0;
+      allRows[rowIndex].classList.add('selected');
+      console.log(rowIndex);
+      break;
+    case 'ArrowDown':
+      console.log(allRows.length);
+      rowIndex =
+        rowIndex < allRows.length - 1 ? rowIndex + 1 : allRows.length - 1;
+      allRows[rowIndex].classList.add('selected');
+      break;
+    case ' ':
+      allRows[rowIndex].classList.add('selected');
+      resetAllActive();
+      allRows[rowIndex].querySelector('img').classList.toggle('active');
+      updateActiveIMG();
+      allRows[rowIndex].parentNode.querySelector('p').classList.toggle('active');
+      break;
+  }
+});
+allRows.forEach((row) => {
+  row.addEventListener('mouseover', () => {
+    allRows.forEach((row) => {
+      row.classList.remove('selected');
+    });
+    row.classList.add('selected');
+  });
+  row.addEventListener('mouseout', () => {
+    row.classList.remove('selected');
+  });
+});
 allImageBtn.forEach((card) => {
   card.addEventListener('click', (e) => {
     if (e.target.tagName === 'IMG') {

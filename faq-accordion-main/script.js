ca507de652fe555344ec42c3079327a2185c55b7
vcgmuse <vcgmuse@gmail.com>
2023-12-26 22:22:20 -0600

    Fixed issues with overcoding

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,92 +1,34 @@
-const allImageBtn = document.querySelectorAll('.card');
-const allRows = document.querySelectorAll('.row');
-let rowIndex = 0;
-addEventListeners();
+const allButtons = document.querySelectorAll('.row button');
+const allImages = document.querySelectorAll('.row img');
+allButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.classList.toggle('active');
+    e.target.nextElementSibling.classList.toggle('active');
 
-function addEventListeners() {
-  allImageBtn.forEach((card) => {
-    card.addEventListener('click', (e) => {
-      handleImgActive(card, e.target);
+    let newArray = Array.from(allButtons).filter(btn=>btn!==e.target);
+    newArray.forEach((element) => {
+      element.parentNode.parentNode.classList.remove('active');
+      element.nextElementSibling.classList.remove('active');
       updateActiveIMG();
     });
-  });
 
-  allRows.forEach((row) => {
-    row.addEventListener('mouseover', () => {
-      row.classList.add('selected');
+  });
+});
+allImages.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.classList.toggle('active');
+    e.target.classList.toggle('active');
+
+    let newArray = Array.from(allImages).filter(img=>img!==e.target);
+    newArray.forEach((element) => {
+      element.parentNode.parentNode.classList.remove('active');
+      element.classList.remove('active');
+      updateActiveIMG();
     });
-    row.addEventListener('mouseout', () => {
-      row.classList.remove('selected');
-    });
+
+    updateActiveIMG();
   });
-
-  document.addEventListener('keydown', (e) => {
-    e.preventDefault();
-    switchKeys(e.key);
-  });
-}
-
-function handleImgActive(card, target) {
-  if (target.tagName === 'IMG') {
-    if (target.classList.contains('active')) {
-      target.classList.toggle('active');
-      card.querySelector('p').classList.toggle('active');
-    } else {
-      resetAllActive();
-      target.classList.toggle('active');
-      card.querySelector('p').classList.toggle('active');
-    }
-  }
-}
-function resetAllActive() {
-  let allActive = document.querySelectorAll('.active');
-  allActive.forEach((active) => {
-    active.classList.toggle('active');
-  });
-}
-function resetAllSelected(){
-  let allSelected = document.querySelectorAll('.selected');
-  allSelected.forEach((selected)=>{
-    selected.classList.remove('selected');
-  })
-}
-
-function switchKeys(key) {
-  switch (key) {
-    case 'ArrowUp':
-      resetAllSelected();
-      rowIndex = rowIndex > 1 ? rowIndex - 1 : 0;
-      allRows[rowIndex].classList.add('selected');
-      break;
-    case 'ArrowDown':
-      resetAllSelected();
-      rowIndex = rowIndex < allRows.length - 1 ? rowIndex + 1 : allRows.length - 1;
-      allRows[rowIndex].classList.add('selected');
-      break;
-    case ' ':
-      // resetAllActive();
-      allRows[rowIndex].classList.add('selected');
-      if(allRows[rowIndex].querySelector('img').classList.contains('active')){
-        allRows[rowIndex].querySelector('img').classList.remove('active');
-        allRows[rowIndex].parentNode
-        .querySelector('p')
-        .classList.remove('active');
-      } else {
-        allRows[rowIndex].querySelector('img').classList.add('active');
-        allRows[rowIndex].parentNode
-        .querySelector('p')
-        .classList.add('active');
-      }
-      allRows.forEach(row=>{
-        if(row===allRows[rowIndex]){
-          
-        }
-      })
-      updateActiveIMG();      
-      break;
-  }
-}
-
+});
 function updateActiveIMG() {
   let allActiveIMG = document.querySelectorAll('.card .row img');
   allActiveIMG.forEach((imgElement) => {

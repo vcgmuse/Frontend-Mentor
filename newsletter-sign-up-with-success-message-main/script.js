3df45a33f7ddafb35d6f8506addf7aec218d5f41
vcgmuse <vcgmuse@gmail.com>
2023-12-30 13:36:38 -0600

    issues with firefox

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -19,7 +19,8 @@ submitBtn.addEventListener('click', (e) => {
     // document.querySelector('.header').style.display = 'none';
     // document.querySelector('.content').style.display = 'none';
     document.querySelector('.success').style.display = 'flex';
-    document.querySelector('.email-target').textContent = emailInput.toLowerCase();
+    document.querySelector('.email-target').textContent =
+      emailInput.toLowerCase();
   }
 });
 
@@ -31,10 +32,13 @@ dismiss.addEventListener('click', (e) => {
   document.querySelector('.success').style.display = 'none';
 });
 let userAgent = navigator.userAgent;
-if (userAgent.indexOf("Firefox") !== -1) {
-  // Inject a style block with a media query for Firefox
-  let cssRules = '@media screen and (min-width: 1440px) { main { margin: 10rem 20rem; } }' ;
-  let style = document.createElement('style');
-  style.textContent = cssRules;
-  document.head.appendChild(style);
-}
+document.onload = function () {
+  if (userAgent.indexOf('Firefox') !== -1) {
+    // Inject a style block with a media query for Firefox
+    let cssRules =
+      '@media screen and (min-width: 1440px) { main { margin: 10rem 20rem; } }';
+    let style = document.createElement('style');
+    style.textContent = cssRules;
+    document.head.appendChild(style);
+  }
+};
